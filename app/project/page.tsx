e2295dd9bee34d3a031bbb7cdbb9c1cb8f8abffe
puckvandiesen <puckvandiesen@gmail.com>
2026-02-02 11:38:26 +0100

    Add files via upload

---
+++ app/project/page.tsx
@@ -0,0 +1,17 @@
+export default function AboutPage() {
+  return (
+    <div className="max-w-4xl mx-auto space-y-6">
+      <h1 className="text-3xl font-bold">About Us</h1>
+      <p className="text-gray-700">
+        We are a company dedicated to providing the best solutions for your needs. 
+        Our team is passionate about creating modern, scalable, and user-friendly applications.
+      </p>
+      <ul className="list-disc pl-5 text-gray-700 space-y-2">
+        <li>Innovative technology</li>
+        <li>Professional team</li>
+        <li>Customer satisfaction</li>
+      </ul>
+      <p> Lorem ipsum dolor sit amet. Vel aliquam modi sed itaque alias sed ipsam vitae qui enim cumque ea sint molestiae ut architecto officia sed iure debitis. A placeat odit aut fuga omnis non consequuntur suscipit et alias suscipit quo doloribus galisum et ullam laboriosam nam galisum laboriosam. Qui reiciendis molestiae non enim incidunt sit neque eligendi. In necessitatibus sunt id illum velit aut beatae sint est nihil expedita. </p>
+    </div>
+  )
+}
