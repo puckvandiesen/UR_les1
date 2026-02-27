899af1dabaeaeb2d35d906d6ed7b0a7d67260936
puckvandiesen <puckvandiesen@gmail.com>
2026-02-27 11:28:21 +0100

    Add files via upload

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,20 +5,27 @@ export default function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="w-full bg-gray-100 text-center py-4 text-sm text-gray-600">
-      <div className="mb-2">
-        &copy; {currentYear} Your Company. All rights reserved.
-      </div>
-      <div className="flex justify-center space-x-4">
-        <Link href="/terms-of-agreement" className="underline hover:text-gray-800">
-          Terms of Agreement
-        </Link>
-        <Link href="/copyright-regulations" className="underline hover:text-gray-800">
-          Copyright Regulations
-        </Link>
-        <Link href="/cookie-settings" className="underline hover:text-gray-800">
-          Cookie Settings
-        </Link>
+    <footer className="w-full bg-gray-100 py-4 text-sm text-gray-600">
+      <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
+        
+        {/* Left side */}
+        <div>
+          &copy; {currentYear} Your Company. All rights reserved.
+        </div>
+
+        {/* Right side */}
+        <div className="flex space-x-6">
+          <Link href="/terms-of-agreement" className="hover:text-gray-800">
+            Terms of Agreement
+          </Link>
+          <Link href="/copyright-regulations" className="hover:text-gray-800">
+            Copyright Regulations
+          </Link>
+          <Link href="/cookie-settings" className="hover:text-gray-800">
+            Cookie Settings
+          </Link>
+        </div>
+
       </div>
     </footer>
   )
